9fc2c8b7d3bb07941c1d0999e6caee7b04265a96
Maciej Adwent <maciekadwent@gmail.com>
2014-02-02 08:24:13 -0800

    formatting cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var transformations = require('./lib/transformations');
 
-module.exports = function(text){
-  return text.split('').reverse().map(function(ch){
+module.exports = function(text) {
+  return text.split('').reverse().map(function(ch) {
     return transformations.hasOwnProperty(ch) ? transformations[ch] : ch;
   }).join('');
 };
